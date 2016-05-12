12dd6c6b92f58aa64ba0075beaf35bfeb1757772
Your Name <you@example.com>
2016-05-12 16:25:56 +0700

    finished index page of posts

--- resources/views/vtv/index.blade.php
+++ resources/views/vtv/index.blade.php
@@ -11,17 +11,22 @@
             <th>YT_id</th>
             <th>Category</th>
             <th>Date Create</th>
+            <th>Operation</th>
         </tr>
     </thead>
     <tbody>
         @foreach ($items as $item)
             <tr>
                 <td>{{ $item->id }}</td>
-                <td>{{ str_limit($item->title, 50) }}</td>
-                <td><a href="{{ $item->link }}" target="_blank">{{ str_limit($item->link, 40) }}</td>
+                <td>{{ str_limit($item->title, 40) }}</td>
+                <td><a href="{{ $item->link }}" target="_blank">{{ str_limit($item->link, 30) }}</td>
                 <td>{{ $item->yt_id }}</td>
                 <td>{{ $item->category_id }}</td>
                 <td>{{ $item->Ins }}</td>
+                <td>
+                    <a href="{{ route('posts.edit', ['id' => $item->id]) }}" class="btn btn-primary" target="_blank">Edit</a>
+                    <a href="{{ route('posts.show', ['id' => $item->id]) }}" class="btn btn-default" target="_blank">Show</a>
+                </td>
             </tr>
         @endforeach
         </tbody>
