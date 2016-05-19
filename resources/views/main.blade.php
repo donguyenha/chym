adc59e7a6876da165b95816eb5aa931e89424f4e
Your Name <you@example.com>
2016-05-19 09:48:08 +0700

    edit navbar

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -19,7 +19,38 @@
 <![endif]-->
   </head>
   <body>
+      <header>
+          <div class="row">
+              <nav class="navbar navbar-default">
+                  <!-- Brand and toggle get grouped for better mobile display -->
+                  <div class="navbar-header">
+                      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                          <span class="sr-only">Toggle navigation</span>
+                          <span class="icon-bar"></span>
+                          <span class="icon-bar"></span>
+                          <span class="icon-bar"></span>
+                      </button>
+                      <!--a class="navbar-brand alonews-brand" href="#"><i class="glyphicon glyphicon-home"></i><span class="sr-only">(current)</span></a-->
+                  </div>
+
+                  <!-- Collect the nav links, forms, and other content for toggling -->
+                  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                      <ul class="nav navbar-nav">
+                          <li class="active"><a href="/"><i class="glyphicon glyphicon-home"></i> <span class="sr-only">(current)</span></a></li>
+                          <li><a href="/home">News</a></li>
+                          <li><a href="#"">Champion League</a></li>
+                          <li><a href="#">Premier League</a></li>
+                          <li><a href="#">Liga BBVA</a></li>
+                          <li><a href="#">Serie A</a></li>
+                          <li><a href="#">Bundesliga</a></li>
+                          <li><a href="#">Ligue One</a></li>
+                      </ul>
+                  </div><!-- /.navbar-collapse -->
+              </nav>
+          </div><!-- /.container-fluid -->
+      </header>
     <div class="container">
+
         @yield('content')
         <hr>
         <p class='text-center'>
